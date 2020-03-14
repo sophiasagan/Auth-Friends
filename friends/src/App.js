bc3ec2b374576ff3d08ca979ef200ea8c753f0e8
sophiasagan <sophiabraddock82@gmail.com>
2020-03-13 23:23:40 -0400

    mvp

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Route, Switch, BrowserRouter as Router} from 'react-router-dom'
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core'
+import {Container, Paper} from '@material-ui/core'
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import './App.css';
+
+//components
+import Login from './components/Login'
+import Public from './components/Public'
+import FriendList from './components/FriendList'
+import PrivateRoute from './components/PrivateRoute'
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+  
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+       <CssBaseline />
+    {/* <Paper elevation={3} > */}
+    <Router>
+      
+      <Container maxWidth='false'>
+
+        <Switch>
+          <PrivateRoute path="/FriendList" component={FriendList} />
+          <Route exact path="/" component={Public} />
+          <Route path="/login" component={Login} />
+        </Switch>
+
+      </Container>
+    </Router>
+    {/* </Paper> */}
+    
+    </MuiThemeProvider>
   );
 }
 
